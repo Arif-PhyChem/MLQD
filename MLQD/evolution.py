23b124e37197f59d3a9368c7a20bc9383f747bea
Arif-PhyChem <88834601+Arif-PhyChem@users.noreply.github.com>
2023-02-03 22:00:10 +0800

    Add files via upload

--- MLQD/evolution.py
+++ MLQD/evolution.py
@@ -103,9 +103,76 @@ class quant_dyn:
 #################################################################################################
                 if self._QDmodelType == 'OSTL' or self._QDmodelType == 'AIQD':
                     if self._QDmodel == 'useQDmodel':
-                        [self._energyDiff, self._Delta, self._gamma, self._lamb, self._temp,
-                        self._energyNorm, self._DeltaNorm, self._gammaNorm, self._lambNorm, 
-                        self._tempNorm] = useQD.loadparam(self._systemType, **param)
+                        if param.get('XfileIn') is None:
+                            if self._systemType == 'SB':
+                                if param.get('energyDiff') is not None:
+                                    self._energyDiff = param.get('energyDiff')
+                                    print('Setting energy difference between two states "energyDiff" to ' + str(self._energyDiff))
+                                else:
+                                    self._energyDiff = 0.0
+                                    print('As energy difference is not provided, the ML-QD is running with the dafault option energyDiff = 0.0')
+                                if param.get('Delta') is not None:
+                                    self._Delta = param.get('Delta')
+                                    print('Setting tunneling matrix element of the two states "Delta" to ' + str(self._Delta))
+                                else:
+                                    self._Delta = 1.0
+                                    print('As tunneling is not provided, the ML-QD is running with the dafault option Delta = 1.0')
+                            else:
+                                self._energyDiff = None
+                                self._Delta = None
+                        
+                            if param.get('gamma') is not None:
+                                self._gamma = param.get('gamma')
+                                print('Setting cutt-off frequency "gamma" to ' + str(self._gamma))
+                            else:
+                                if self._systemType == 'FMO':
+                            	    self._gamma = 500
+                            	    print('Running with the default value of cutt-off frequency; gamma = 500')
+                                if self._systemType == 'SB':
+                            	    self._gamma = 10
+                            	    print('Running with the default value of cutt-off frequency; gamma = 10')
+                            
+                            if param.get('lamb') is not None:
+                                self._lamb = param.get('lamb')
+                                print('Setting system-bath coupling strength "lambda" to ' + str(self._lamb))
+                            else:
+                                if self._systemType == 'FMO':
+                                    self._lamb = 520
+                                    print('Running with the default value of system-bath coupling strength; lamb = 520')
+                                if self._systemType == 'SB':
+                                    self._lamb = 1.0
+                                    print('Running with the default value of system-bath coupling strength; lamb = 0.1')
+                            if param.get('temp') is not None:
+                                self._temp = param.get('temp')
+                                print('Setting temperature (or inverse temperature) value "temp" to ' + str(self._temp))
+                            else:
+                                if self._systemType == 'FMO':
+                                    self._temp = 510
+                                    print('Running with the default temperature (or inverse temperature) value temp = 510')
+                                if self._systemType == 'SB':
+                                    self._temp = 1.0
+                                    print('Running with the default temperature (or inverse temperature) value temp = 1.0')
+                                print('=================================================================')
+                            if param.get('QDmodelIn') is not None:
+                                self._QDmodelIn = param.get('QDmodelIn')
+                            else:
+                                raise ValueError(str(self._QDmodelIn) + '.pkl does not exist')
+                            name = re.split(r'.hdf5', self._QDmodelIn)[0] + ".pkl"
+                            print('Reading normalization constants from', name)
+                            print('=================================================================')
+                            f = open(name, 'rb')   # Load normalization parameters
+                            norm_param = pickle.load(f)
+                            f.close()
+                            self._energyNorm = norm_param['energyNorm']
+                            self._DeltaNorm = norm_param['DeltaNorm']
+                            self._lambNorm = norm_param['lambNorm']
+                            self._gammaNorm = norm_param['gammaNorm']
+                            self._tempNorm = norm_param['tempNorm']
+                            print('Setting energy difference normalizer "energyNorm" to ' + str(self._energyNorm))
+                            print('Setting tunneling matrix element normalizer "DeltaNorm" to ' + str(self._DeltaNorm))
+                            print('Setting gamma normalizeer "gammaNorm" to ' + str(self._gammaNorm))
+                            print('Setting lambda normalizer "lambNormalizer" to ' + str(self._lambNorm))
+                            print('Setting temperature (or inverse temperature) normalizer "tempNorm" to ' + str(self._tempNorm))
                     if self._QDmodel == 'useQDmodel':
                         if self._systemType == 'FMO':
                             if self._n_states == 7:
