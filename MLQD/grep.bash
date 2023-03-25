af03dbe9d4b4db76c15ea79cd67bcee5916be1f0
Arif-PhyChem <88834601+Arif-PhyChem@users.noreply.github.com>
2023-03-25 21:53:06 +0800

    Delete grep.bash

--- MLQD/grep.bash
+++
@@ -1,7 +0,0 @@
-if grep -q "FINAL ML MODEL CREATED AND SAVED" $PWD/kkr_train_output; then 
-	echo "grep.bash: MLatom execution was successful, Cheers!"
-else
-	echo "grep.bash: MLatom execution was not successful, Check your input files etc. !"
-fi
-
-
